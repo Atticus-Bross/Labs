8aa53dcc5ed77e793b61b7f65e1767353b5882e6
Atticus Bross <atb5098@utulsa.edu>
2024-10-08 16:35:29 -0500

    added test function for seq_comb

---
+++ test_lab6.py
@@ -0,0 +1,8 @@
+from lab6 import *
+def test_seq_comb()->None:
+    """test_seq_com() Tests the seq_comb function"""
+    assert seq_comb('abc',lambda x,y:x+y)==('ab','ac','bc')
+    assert seq_comb((1,2,3),lambda x,y:x+y)==(3,4,5)
+    assert seq_comb({'a':1,'b':2,'c':3},lambda x,y:x-y)==(-1,-2,-1)
+    assert seq_comb([1,2,3],lambda x,y:x*y)==(2,3,6)
+test_seq_comb()
