7bc1852ad168f7fb8cf1fb122322c49487b967f7
Atticus Bross <atb5098@utulsa.edu>
2024-10-09 09:30:28 -0500

    added test for fetch function

--- test_lab6.py
+++ test_lab6.py
@@ -16,4 +16,11 @@ def test_seq_comb()->None:
     assert seq_comb({'a':1,'b':2,'c':3},lambda x,y:x-y)==(-1,-2,-1)
     assert seq_comb([1,2,3],lambda x,y:x*y)==(2,3,6)
     assert seq_comb({'a':1,'b':1,'c':1},lambda x,y:x-y)==(0,0,0)
+def test_fetch()->None:
+    """test_fetch() Tests the fetch function"""
+    assert fetch('abc',0,1)==('a','b')
+    assert fetch({1:2,3:4,5:6},3,5)==(4,6)
+    assert fetch([1,2,3,4,5],0,2,4)==(1,3,5)
+    assert fetch((1,2,3,4,5),1,3)==(2,4)
 test_seq_comb()
+test_fetch()
