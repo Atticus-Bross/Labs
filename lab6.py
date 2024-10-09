91d1e1ffebb09f8213448a54b1f295284d09f21d
Atticus Bross <atb5098@utulsa.edu>
2024-10-09 15:22:21 -0500

    removed seq_comb, temp_variance and tests

--- lab6.py
+++ lab6.py
@@ -4,26 +4,6 @@ Number=int|float
 Sequence=str|list|tuple|dict
 with open('datasets/counties.json', 'r') as jsonfile:
     data = load(jsonfile)
-def seq_comb(seq:Sequence, f)->tuple:
-    """seq_comb(seq, f) Evaluates a function on every unique combination of two elements in seq
-
-    seq: the sequence the operation is to be preformed on
-    f: a function that takes two parameters"""
-    if len(seq)<2:
-        raise ValueError('seq should be at least two length')
-    return_values:tuple=()
-    #dictionaries require special handling
-    if type(seq)==dict:
-        seq=seq.values()
-    #exclusions needs to use list methods
-    exclusions:list=list(seq)
-    for value_outer in seq:
-        # the identity not the order of elements matters
-        # all the remaining combinations with value_outer in them will be tried in the next inner loop
-        exclusions.remove(value_outer)
-        for value_inner in exclusions:
-            return_values=return_values+(f(value_outer,value_inner),)
-    return return_values
 def fetch(seq:Sequence,*indexes)->tuple:
     """fetch(seq, *indexes) retrieves specific elements from a sequence
 
@@ -33,13 +13,6 @@ def fetch(seq:Sequence,*indexes)->tuple:
     for index in indexes:
         return_tuple=return_tuple+(seq[index],)
     return return_tuple
-def temp_variance(county_index:int)->Number:
-    """temp_variance(county_index) Gives the average temperature variance of a county over a year
-
-    county_index: the index where the county is located in counties.json"""
-    temps:tuple =fetch(data[county_index]['noaa'],'temp-jan','temp-apr','temp-jul','temp-oct')
-    temps_variance:tuple=seq_comb(temps,lambda x,y:abs(x-y))
-    return average(temps_variance)
 def header(text:str,level:int, file)->None:
     """header(text, level, file) Adds a header to a markdown file
 
--- test_lab6.py
+++ test_lab6.py
@@ -1,37 +1,12 @@
 from lab6 import *
 from math import isclose
-def test_seq_comb()->None:
-    """test_seq_com() Tests the seq_comb function"""
-    #if the function manages to execute an assertion error will be raised
-    try:
-        seq_comb((),lambda x:x)
-        assert False
-    except ValueError:
-        pass
-    try:
-        seq_comb({1:2},lambda x:x)
-        assert False
-    except ValueError:
-        pass
-    assert seq_comb('abc',lambda x,y:x+y)==('ab','ac','bc')
-    assert seq_comb((1,2,3),lambda x,y:x+y)==(3,4,5)
-    assert seq_comb({'a':1,'b':2,'c':3},lambda x,y:x-y)==(-1,-2,-1)
-    assert seq_comb([1,2,3],lambda x,y:x*y)==(2,3,6)
-    assert seq_comb({'a':1,'b':1,'c':1},lambda x,y:x-y)==(0,0,0)
 def test_fetch()->None:
     """test_fetch() Tests the fetch function"""
     assert fetch('abc',0,1)==('a','b')
     assert fetch({1:2,3:4,5:6},3,5)==(4,6)
     assert fetch([1,2,3,4,5],0,2,4)==(1,3,5)
     assert fetch((1,2,3,4,5),1,3)==(2,4)
-def test_temp_variance()->None:
-    """test_temp_variance() Tests the temp_variance function"""
-    assert isclose(temp_variance(0),27.3)
-    assert isclose(temp_variance(1),26.683333333333334)
-    assert isclose(temp_variance(2),26.05)
-# test_seq_comb()
 # test_fetch()
-# test_temp_variance()
 #test file writing functions
 with open('files for writing/test.md','w') as mdfile:
     header('test',1,mdfile)
