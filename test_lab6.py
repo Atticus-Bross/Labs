a04ec67b6fbb67d8b8b34c22ea5c7bf5adcaa272
Atticus Bross <atb5098@utulsa.edu>
2024-10-09 09:01:11 -0500

    added test case for duplicate values to test_seq_comb

--- test_lab6.py
+++ test_lab6.py
@@ -5,4 +5,5 @@ def test_seq_comb()->None:
     assert seq_comb((1,2,3),lambda x,y:x+y)==(3,4,5)
     assert seq_comb({'a':1,'b':2,'c':3},lambda x,y:x-y)==(-1,-2,-1)
     assert seq_comb([1,2,3],lambda x,y:x*y)==(2,3,6)
+    assert seq_comb({'a':1,'b':1,'c':1},lambda x,y:x-y)==(0,0,0)
 test_seq_comb()
