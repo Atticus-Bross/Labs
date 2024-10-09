157092d5e9b518f2690c1211e401f4b10de407df
Atticus Bross <atb5098@utulsa.edu>
2024-10-09 09:06:23 -0500

    added test cases for value errors

--- test_lab6.py
+++ test_lab6.py
@@ -1,6 +1,16 @@
 from lab6 import *
 def test_seq_comb()->None:
     """test_seq_com() Tests the seq_comb function"""
+    try:
+        seq_comb((),lambda x:x)
+        assert False
+    except ValueError:
+        pass
+    try:
+        seq_comb({1:2},lambda x:x)
+        assert False
+    except ValueError:
+        pass
     assert seq_comb('abc',lambda x,y:x+y)==('ab','ac','bc')
     assert seq_comb((1,2,3),lambda x,y:x+y)==(3,4,5)
     assert seq_comb({'a':1,'b':2,'c':3},lambda x,y:x-y)==(-1,-2,-1)
