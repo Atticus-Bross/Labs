71b796969ad954df7e468c92beeadde9db0d5b46
Atticus Bross <atb5098@utulsa.edu>
2024-10-09 11:26:54 -0500

    added test for temp_variance

--- test_lab6.py
+++ test_lab6.py
@@ -1,4 +1,5 @@
 from lab6 import *
+from math import isclose
 def test_seq_comb()->None:
     """test_seq_com() Tests the seq_comb function"""
     try:
@@ -22,5 +23,11 @@ def test_fetch()->None:
     assert fetch({1:2,3:4,5:6},3,5)==(4,6)
     assert fetch([1,2,3,4,5],0,2,4)==(1,3,5)
     assert fetch((1,2,3,4,5),1,3)==(2,4)
+def test_temp_variance()->None:
+    """test_temp_variance() Tests the temp_variance function"""
+    assert isclose(temp_variance(0),27.3)
+    assert isclose(temp_variance(1),26.683333333333334)
+    assert isclose(temp_variance(2),26.05)
 # test_seq_comb()
 # test_fetch()
+test_temp_variance()
