78282884b2f3ac76e922d8d3fcbb1caecb23dce1
Atticus Bross <atb5098@utulsa.edu>
2024-10-15 19:11:45 -0500

    added table_from_list function

--- lab7.py
+++ lab7.py
@@ -7,7 +7,7 @@ Creating Zipper Merges
 Applying a Caesar Cypher to Text
 
 Completed by Atticus Bross on 2024-10-22 for DS-1043"""
-def same_len_error(seq:list[list,...]|list[dict,...],error_txt:str)->None:
+def same_len_error(seq:list[list]|list[dict],error_txt:str)->None:
     """same_len_error(seq, error_txt)
     Raises a ValueError if the sequences within seq are not all the same length
 
@@ -16,23 +16,33 @@ def same_len_error(seq:list[list,...]|list[dict,...],error_txt:str)->None:
     lengths:list=list(map(len,seq))
     if lengths.count(lengths[0])!=len(lengths):
         raise ValueError(error_txt)
-def create_table(header:list|dict,data:list[list,...]|list[dict,...])->list[str,...]:
+def table_from_list(header:list,data:list[list])->list[str]:
+    """table_from_list(header, data)
+    Creates a table from a header and data that are lists of values
+
+    header: the headers
+    data: the values of the table"""
+    fixed:'map'=map(fix,data)
+    col:list=columns(header,fixed)
+    #type of the elements within the columns
+    col_types:'map'=map(lambda x:type(x[1]),col)
+    aligns:'map'=map(alignment,col_types)
+    widths:'map'=map(min_width,col)
+    unpacked:list=extract(header,fixed)
+    pass_values:'map'=map(str,unpacked)
+    table(pass_values,aligns,widths)
+def create_table(header:list|dict,data:list[list]|list[dict])->list[str]:
     """create_table(header, data)
     Creates a list of strings describing the rows of a Markdown table
 
     header: a list of headers
     data: a list of the data values"""
-    if len(header)<1:
-        raise ValueError('there must be at least one header')
-    if len(data)<1:
-        raise ValueError('there must be at least one data point')
     same_len_error(data,'all rows must be the same length')
-    data_type:type=check_data_type(data)
+    data_type:type=type(data[0])
     #whether all the sequence in data are the same length has already been checked
     if data_type==list and len(header)!=len(data[0]):
         raise ValueError('there must be as many headers as there are columns')
-    col:list=columns(header,data)
-    col_types:'map'=map(type,col)
-    aligns:'map'=map(alignment,col_types)
-    widths:'map'=map(min_width,col)
-    return table_text(col,aligns,widths)
+    if data_type==list:
+        return table_from_list(header,data)
+    elif data_type==dict:
+        return table_from_dict(header,data)
--- test_lab7.py
+++ test_lab7.py
@@ -3,14 +3,28 @@ Module Testing the Lab 7 functions
 
 Completed by Atticus Bross on 2024-10-22 for DS-1043"""
 from typing import Callable
+
+from lab7 import same_len_error
+
+
 def test_error(error_type:type,f:Callable)->None:
     """test_error()
     Tests if a function gives the correct error
 
     error_type: the type of error expected
-    f: the function, should be specified as lambda x: f(desired parameters)"""
+    f: the function, should be specified as lambda: f(desired parameters)"""
     try:
         f()
         assert False
     except error_type:
         pass
+def test_same_len_error()->None:
+    """test_same_len_error()
+    Tests the same_len_error function"""
+    same_len_error([[], [], []], '')
+    same_len_error([[1,2],['a','b'],[True,False]],'')
+    same_len_error([[1,], ['a',], [True,]], '')
+    test_error(ValueError,lambda:same_len_error([[],[1]],''))
+    test_error(ValueError,lambda:same_len_error([[1,2],[3,4,5]],''))
+    test_error(ValueError, lambda: same_len_error([[1, 2,3,4], [3, 4, 5]], ''))
+test_same_len_error()
