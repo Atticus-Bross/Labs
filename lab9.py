be2e9d887741cdc21d4deb24834706ae8fdd9a68
Atticus Bross <atb5098@utulsa.edu>
2024-10-29 16:45:21 -0500

    reformatted the code

--- lab9.py
+++ lab9.py
@@ -6,43 +6,44 @@ Completed by Atticus Bross on 2024/11/09 for DS-1043"""
 import inspect
 from collections import defaultdict
 
-
-_FUDGE = 1 # comp returns all matches with length >= longest match - fudge
+_FUDGE = 1  # comp returns all matches with length >= longest match - fudge
 
 
 def ngrams(word: str) -> list[str]:
     """Returns a list of n-grams of word for all relevant n, in descending order of n."""
-    grams:list=[]
-    for length in range(len(word),0,-1):
-        for start in range(0,len(word)-length+1):
-            grams.append(word[start:start+length])
+    grams: list = []
+    for length in range(len(word), 0, -1):
+        for start in range(0, len(word) - length + 1):
+            grams.append(word[start:start + length])
     return grams
+
 
 def add_to_index(option: str, index: dict[str, list[str]]) -> None:
     """Adds a valid option to the n-gram index."""
-    grams:list=ngrams(option)
+    grams: list = ngrams(option)
     for gram in grams:
         index[gram].append(option)
+
 
 def build_index(options: list[str]) -> dict[str, list[str]]:
     """Creates an n-gram index from options.
     The n-gram index will be a dictionary with n-grams as keys, and lists of corrosponding options as values."""
     index = defaultdict(list)
     for option in options:
-        add_to_index(option,index)
+        add_to_index(option, index)
     return index
 
 
-def fuzzy_pick(query: str, index: dict) -> dict[str,str]:
+def fuzzy_pick(query: str, index: dict) -> dict[str, str]:
     """Returns suggestions for valid options based on the query string.
     Suggestions will take the form of a dictionary with suggestions as keys and longest matching ngram as the value"""
     suggestions = {}
-    grams:list=ngrams(query)
+    grams: list = ngrams(query)
     for gram in grams:
         if gram in index.keys():
             for match in index[gram]:
                 if match not in suggestions.keys():
-                    suggestions[match]=gram
+                    suggestions[match] = gram
     return suggestions
 
 
--- test_lab9.py
+++ test_lab9.py
@@ -4,31 +4,48 @@ Tests the Lab 9 functions
 
 Completed by Atticus Bross on 2024/11/09 for DS-1043"""
 from lab9 import *
-def test_ngrams()->None:
+
+
+def test_ngrams() -> None:
     """test_ngrams()
     Tests the ngrams function"""
-    assert ngrams('as')==['as','a','s']
-    assert ngrams('the')==['the','th','he','t','h','e']
-    assert ngrams('a')==['a']
-def test_add_to_index()->None:
+    assert ngrams('as') == ['as', 'a', 's']
+    assert ngrams('the') == ['the', 'th', 'he', 't', 'h', 'e']
+    assert ngrams('a') == ['a']
+
+
+def test_add_to_index() -> None:
     """test_add_to_index()
     Tests the add_to_index function"""
-    test:dict=defaultdict(list)
-    add_to_index('as',test)
-    assert test=={'a':['as'],'s':['as'],'as':['as']}
+    test: dict = defaultdict(list)
+    add_to_index('as', test)
+    assert test == {'a': ['as'], 's': ['as'], 'as': ['as']}
     add_to_index('tas', test)
-    assert test=={'a':['as','tas'],'s':['as','tas'],'as':['as','tas'],'t':['tas'],'ta':['tas'],'tas':['tas']}
-def test_build_index()->None:
+    assert test == {'a': ['as', 'tas'], 's': ['as', 'tas'], 'as': ['as', 'tas'], 't': ['tas'], 'ta': ['tas'],
+                    'tas': ['tas']}
+
+
+def test_build_index() -> None:
     """test_build_index()
     Tests the build_index function"""
-    assert build_index(['a'])=={'a':['a']}
-    assert build_index(['as','tas'])=={'a':['as','tas'],'s':['as','tas'],'as':['as','tas'],'t':['tas'],'ta':['tas'],'tas':['tas']}
-    assert build_index(['a','as','tas'])=={'a':['a','as','tas'],'s':['as','tas'],'as':['as','tas'],'t':['tas'],'ta':['tas'],'tas':['tas']}
-def test_fuzzy_pick()->None:
+    assert build_index(['a']) == {'a': ['a']}
+    assert build_index(['as', 'tas']) == {'a': ['as', 'tas'], 's': ['as', 'tas'], 'as': ['as', 'tas'], 't': ['tas'],
+                                          'ta': ['tas'], 'tas': ['tas']}
+    assert build_index(['a', 'as', 'tas']) == {'a': ['a', 'as', 'tas'], 's': ['as', 'tas'], 'as': ['as', 'tas'],
+                                               't': ['tas'], 'ta': ['tas'], 'tas': ['tas']}
+
+
+def test_fuzzy_pick() -> None:
     """test_fuzzy_pick()
     Tests the fuzzy_pick function"""
-    assert fuzzy_pick('the',{'a':['a','as','tas'],'s':['as','tas'],'as':['as','tas'],'t':['tas'],'ta':['tas'],'tas':['tas']})=={'tas':'t'}
-    assert fuzzy_pick('sa',{'a':['a','as','tas'],'s':['as','tas'],'as':['as','tas'],'t':['tas'],'ta':['tas'],'tas':['tas']})=={'as':'s','tas':'s','a':'a'}
+    assert fuzzy_pick('the',
+                      {'a': ['a', 'as', 'tas'], 's': ['as', 'tas'], 'as': ['as', 'tas'], 't': ['tas'], 'ta': ['tas'],
+                       'tas': ['tas']}) == {'tas': 't'}
+    assert fuzzy_pick('sa',
+                      {'a': ['a', 'as', 'tas'], 's': ['as', 'tas'], 'as': ['as', 'tas'], 't': ['tas'], 'ta': ['tas'],
+                       'tas': ['tas']}) == {'as': 's', 'tas': 's', 'a': 'a'}
+
+
 test_ngrams()
 test_add_to_index()
 test_build_index()
