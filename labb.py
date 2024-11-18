41045508736c57d41a2c53b943db9e59c221b22c
Atticus Bross <atb5098@utulsa.edu>
2024-11-18 13:52:40 -0600

    implemented basic node object and insert method

---
+++ labb.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, value, parent=None):
+        self._value = value
+        self._parent = parent
+        self._left = None
+        self._right = None
+        self._quantity = 1
+    def __repr__(self):
+        if self._left is None and self._right is None:
+            return f'{self._value}'
+        return f'{self._value} ({self._left}, {self._right})'
+    def __eq__(self, other):
+        return self._value == other
+    def __gt__(self, other):
+        return self._value > other
+    def __lt__(self, other):
+        return self._value < other
+    def __le__(self, other):
+        return self._value <= other
+    def __ge__(self, other):
+        return self._value >= other
+
+    def insert(self,value):
+        if self._left is None:
+            self._left = Node(value,self)
+        elif value < self:
+            self._left.insert(value)
+        elif self._right is None:
+            self._right = Node(value,self)
+        elif value > self:
+            self._right.insert(value)
+        elif value == self:
+            self._quantity = self._quantity + 1
